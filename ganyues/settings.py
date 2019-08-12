75338c702aed092a29186400a071c704f53751d9
wubounce <18311017120@163.com>
2019-08-12 18:06:52 +0800

    缩进有问题导致connect() failed (111: Connection refused) while connecting to upstream,首先得保证Django项目没有问题

--- ganyues/settings.py
+++ ganyues/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS = [
  #  os.path.join(BASE_DIR, 'static')
 #]
- STATIC_ROOT = '/home/www/ganyues/static/'
+STATIC_ROOT = '/home/www/ganyues/static/'
 # STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 
 
